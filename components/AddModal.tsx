520adc45cb47a79b638f849a19d024e7efbacd8d
Alex Tarnawski <atmarnawski@gmail.com>
2023-07-31 15:50:20 -0700

    add modal

---
+++ components/AddModal.tsx
@@ -0,0 +1,28 @@
+import { createPortal } from "react-dom";
+import { styled } from "styled-components";
+
+const Modal = styled.div`
+  border: 1px solid black;
+  border-radius: 2px;
+  position: absolute;
+  padding: 5px;
+  left: 35px;
+  background-color: white;
+`;
+
+const Input = styled.input`
+ 
+  border 2px solid black
+`;
+
+const Button = styled.button``;
+
+export default function AddModal({ containerRef }) {
+  return createPortal(
+    <Modal>
+      <Input type="text" />
+      <Button>Add</Button>
+    </Modal>,
+    containerRef
+  );
+}
